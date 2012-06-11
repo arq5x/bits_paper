8fbb1f7aa21fdba80c1b71402bc280c773e37f07
Aaron Quinlan <arq5x@virginia.edu>
2012-06-11 12:31:58 -0400

    initial commit

---
+++ Makefile
@@ -0,0 +1,21 @@
+MAIN=bioinformatics
+MPDIR=mp
+#INCLUDES=$(shell egrep '^[^%]*\\input' $(MAIN).tex | sed -e 's/.*{//;s/}.*//')
+#MPS=$(shell egrep '^[^%]*\\includegraphics' $(MAIN).tex $(INCLUDES)| sed -e 's/.*{//;s/}.*//')
+
+#MPS=$(shell ls -1 $(MPDIR)/*.mp)
+
+
+	#mpost $(MPDIR)/$(MAIN)
+
+all: $(MPS)
+	latex $(MAIN).tex
+	bibtex $(MAIN)
+	latex $(MAIN).tex
+	latex $(MAIN).tex
+	dvipdf -dAutoRotatePages=/None $(MAIN).dvi
+
+
+
+clean:
+	rm -f *.aux *.dvi *.log *.pdf *.mpx *.[1-9]
---
+++ figures/exons-v-genome.R
@@ -0,0 +1,24 @@
+library(ggplot2)
+
+d <- read.table('exons-v-genome.txt', header=TRUE)
+
+postscript(file='exons-v-genome.eps',  width = 7.0, height = 8.0,
+           horizontal = FALSE, onefile = FALSE, paper = "special",
+           pointsize=24)
+
+p <- ggplot(d, aes(x = d$size, y = d$speed, fill=d$alg))
+p + geom_bar(position = "dodge", colour='black') + 
+    scale_y_log10(breaks=c(1e0,1e1,1e2,1e3,1e4,1e5,1e6,1e7,1e8)) + 
+    scale_fill_brewer(palette = "Greys") +
+    scale_x_discrete(limits=c("10K","100K","1M","10M")) +
+    opts(panel.background = theme_rect(fill='white')) +
+    opts(panel.grid.major = theme_blank()) +
+    opts(panel.grid.minor = theme_blank()) +
+    opts(axis.text.x = theme_text(size = 14)) +
+    opts(axis.text.y = theme_text(size = 14)) +
+    opts(axis.title.x = theme_text(size = 16)) +
+    opts(axis.title.y = theme_text(size = 16, angle=90)) +
+    opts(legend.title=theme_blank()) +
+    opts(legend.position=c(0.30,0.80)) +
+    labs(x = "Interval database size", y = "Runtime(microseconds)")    
+dev.off()
